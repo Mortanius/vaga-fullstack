d8eae93464f2d8856ba355faa23beb8198db024f
Mortanius <wilneto17@hotmail.com>
2021-02-25 17:18:43 -0300

    Adiciona origins ao CrossOrigin decorator

--- backend/catalogodeleite/src/main/kotlin/com/wmeds/catalogodeleite/controller/ProdutoController.kt
+++ backend/catalogodeleite/src/main/kotlin/com/wmeds/catalogodeleite/controller/ProdutoController.kt
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-@CrossOrigin
+@CrossOrigin(origins = ["http://localhost:4200", "http://localhost:8000"])
 @RestController
 @RequestMapping("/api/produtos")
 class ProdutoController(private val service: ProdutoService) {
